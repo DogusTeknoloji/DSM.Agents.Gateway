9a331cc1ae5b3aad34a03c2fe0a5593a9334f1d2
Onur Akkaya <onur.akkaya@d-teknoloji.com.tr>
2019-11-14 17:40:19 +0300

    Initial Commit

---
+++ GatewayService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DSM.Agents.Gateway
+{
+    public class GatewayService : DSMAgent
+    {
+        public GatewayService() : base("DSMGatewayService", bypassAuth: true) { }
+
+        public override void ActionMain(object authObj)
+        {
+            try
+            {
+
+                GC.TryStartNoGCRegion(1024 ^ 3 * 8);
+                GatewayEngine.Gateway gateway = new DSM.GatewayEngine.Gateway();
+                GC.EndNoGCRegion();
+                GC.KeepAlive(gateway);
+                GC.SuppressFinalize(gateway);
+                loggingSvc.Write($"Gateway ref -> {gateway?.ToString()}");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                loggingSvc.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceProcess;
+
+namespace DSM.Agents.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (GatewayService service = new GatewayService())
+            {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
